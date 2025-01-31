a205ebe1e9bfa088ca25e7e4f09e9bc9ab88a08a
salaheddineva <salaheddine@veniseactivation.com>
2025-01-31 16:08:10 +0100

    Add asset URL configuration and image retrieval route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/resources/images/{filename}', function($filename){
+    $path = resource_path() . '/js/images/' . $filename;
+
+    if(!File::exists($path)) {
+        return response()->json(['message' => 'Image not found.'], 404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
